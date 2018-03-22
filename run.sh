7a417fb3e6df08bfcd49bec49fe3ce9e46dd854e
EC2 Default User <ec2-user@ip-172-31-33-21.us-east-2.compute.internal>
2018-03-22 20:41:42 +0000

    updated ecs cfn parameter files

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 #####
 sh ~/environment/ecs-continous-deployment/bin/deploy ecs-continuous-deployment
 
-aws cloudformation create-stack --stack-name stack-ecs-efs-asg --template-body ecs-continuous-deployment.yaml --parameters ParameterKey=LaunchType,ParameterValue=EC2 ParameterKey=GitHubUser,ParameterValue=kvaibhawa ParameterKey=GitHubRepo,ParameterValue=example-on-ecs ParameterKey=GitHubBranch,ParameterValue=master ParameterKey=GitHubBranch,ParameterValue=master ParameterKey=GitHubToken,ParameterValue=1476a4f217a29ad9a4dc2fd93c736c8b4b009739 ParameterKey=TemplateBucket,ParameterValue=master --capabilities CAPABILITY_NAMED_IAM --region us-east-1
+sh ~/environment/ecs-continous-deployment/bin/createstack.sh stack-ecs-efs-asg 
+##aws cloudformation create-stack --stack-name stack-ecs-efs-asg --template-body ecs-continuous-deployment.yaml --parameters ParameterKey=LaunchType,ParameterValue=EC2 ParameterKey=GitHubUser,ParameterValue=kvaibhawa ParameterKey=GitHubRepo,ParameterValue=example-on-ecs ParameterKey=GitHubBranch,ParameterValue=master ParameterKey=GitHubBranch,ParameterValue=master ParameterKey=GitHubToken,ParameterValue=1476a4f217a29ad9a4dc2fd93c736c8b4b009739 ParameterKey=TemplateBucket,ParameterValue=master --capabilities CAPABILITY_NAMED_IAM --region us-east-1
