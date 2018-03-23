e292fcb2b291c39f0cc8cd62e8aeabf8aa4c78d4
kumar vaibhawa <vaibhawa@amazon.com>
2018-03-23 18:47:26 -0400

    updated readme and run

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 sh <update the project path>/bin/deploy demo-ecs-continuous-deployment
 #####This will fetch the ami ID for all regions.
 sh <update the project path>/bin/get_amis.sh
-###Created stack for ecs continous deployment pipeline
+###Create stack for ecs continous deployment pipeline
 aws cloudformation create-stack --stack-name <name of stack> --template-body file://ecs-continuous-deployment.yaml --parameters ParameterKey=LaunchType,ParameterValue=EC2 ParameterKey=GitHubUser,ParameterValue=kvaibhawa ParameterKey=GitHubRepo,ParameterValue=example-on-ecs ParameterKey=GitHubBranch,ParameterValue=master ParameterKey=GitHubToken,ParameterValue=<github user token> ParameterKey=TemplateBucket,ParameterValue=demo-ecs-continuous-deployment --capabilities CAPABILITY_NAMED_IAM --region us-east-1
